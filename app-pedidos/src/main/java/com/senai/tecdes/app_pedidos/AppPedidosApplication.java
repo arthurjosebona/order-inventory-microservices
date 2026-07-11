d444bb5e067b8e912870091d77b98231d6ce16fe
arthurjosebona <arthurjosebona@gmail.com>
2026-07-11 12:53:21 -0300

    fix: add annotation to handle feign clients

--- app-pedidos/src/main/java/com/senai/tecdes/app_pedidos/AppPedidosApplication.java
+++ app-pedidos/src/main/java/com/senai/tecdes/app_pedidos/AppPedidosApplication.java
@@ -2,8 +2,10 @@ package com.senai.tecdes.app_pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AppPedidosApplication {
 
 	public static void main(String[] args) {
